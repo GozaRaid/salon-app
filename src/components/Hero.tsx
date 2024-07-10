e004540eaf2dfc8f0712a34ff1873bf4873df46f
GozaRaid <hafidzhitobaihaqi@gmail.com>
2024-07-11 01:17:33 +0700

    initial commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import Image from "next/image";
+import { CustomButton } from "@/components";
+
+const Hero = () => {
+  return (
+    <div className="bg-custom-gray relative">
+      <div className="flex xl:flex-row flex-col gap-5 z-0 max-w-[1600px] mx-auto">
+        <div className="flex-1 pt-36 px-6 sm:px-16">
+          <h1 className="2xl:text-[72px] sm:text-[64px] text-[50px] font-extrabold">
+            Beauty and Elegance Redefined
+          </h1>
+
+          <p className="text-[27px] text-black-100 font-light mt-5">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur
+            obcaecati dolore vitae et nobis a reiciendis, suscipit autem nisi
+            impedit quia mollitia voluptatem error officia fugit eum accusamus
+            consectetur id.
+          </p>
+
+          <div className="flex flex-row gap-3">
+            <CustomButton
+              // title="Get started"
+              title="Get to know us"
+              containerStyles="bg-primary-blue text-white rounded-full mt-10"
+              handleClick={() => {}}
+            />
+
+            <CustomButton
+              // title="Make Appointment"
+              title="Get started"
+              containerStyles="bg-primary-blue text-white rounded-full mt-10"
+              handleClick={() => {}}
+            />
+          </div>
+        </div>
+
+        <div className="xl:flex-[1.5] flex justify-center items-center w-full xl:h-screen">
+          <div className="relative xl:w-full w-[90%] xl:h-full h-[590px] z-0 rounded-lg overflow-hidden">
+            <Image
+              src="/hero.jpg"
+              alt="hero"
+              fill
+              className="object-contain object-center"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,174 @@
+"use client";
+
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { CustomButton } from "@/components";
+
+const navigation = [
+  { name: "Home", href: "#", current: false },
+  { name: "Services", href: "#", current: false },
+  { name: "Contact Us", href: "#", current: false },
+  { name: "Branch", href: "#", current: false },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Example() {
+  return (
+    <Disclosure as="nav" className="bg-gray-800">
+      <div className="px-2 sm:px-8 lg:px-8 max-w-[1600px] mx-auto">
+        <div className="relative flex h-20 items-center justify-between">
+          {/* Mobile menu button*/}
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon
+                aria-hidden="true"
+                className="block h-6 w-6 group-data-[open]:hidden"
+              />
+              <XMarkIcon
+                aria-hidden="true"
+                className="hidden h-6 w-6 group-data-[open]:block"
+              />
+            </DisclosureButton>
+          </div>
+
+          <div className="flex flex-1 items-center md:justify-between">
+            <div className="flex flex-shrink-0 items-center">
+              <a href="">
+                <img
+                  alt="Salon Logo"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                  className="h-8 w-auto"
+                />
+              </a>
+            </div>
+            <div className="hidden md:ml-6 md:block">
+              <div className="flex space-x-4">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    aria-current={item.current ? "page" : undefined}
+                    className={classNames(
+                      item.current
+                        ? "bg-gray-900 text-white"
+                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                      "rounded-md px-3 py-2 text-sm font-medium"
+                    )}
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+            <div className="hidden md:ml-6 md:block">
+              <div className="flex space-x-4">
+                <CustomButton
+                  title="Sign in"
+                  containerStyles=" text-white text-sm"
+                  handleClick={() => {}}
+                />
+
+                <CustomButton
+                  title="Sign Up"
+                  containerStyles="text-white text-sm border border-white rounded-full"
+                  handleClick={() => {}}
+                />
+              </div>
+            </div>
+
+            {/* Profile dropdown */}
+            {/* <Menu as="div" className="relative ml-3">
+              <div>
+                <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                  <span className="absolute -inset-1.5" />
+                  <span className="sr-only">Open user menu</span>
+                  <img
+                    alt=""
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    className="h-8 w-8 rounded-full"
+                  />
+                </MenuButton>
+              </div>
+              <MenuItems
+                transition
+                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+              >
+                <MenuItem>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+                  >
+                    Your Profile
+                  </a>
+                </MenuItem>
+                <MenuItem>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+                  >
+                    Settings
+                  </a>
+                </MenuItem>
+                <MenuItem>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+                  >
+                    Sign out
+                  </a>
+                </MenuItem>
+              </MenuItems>
+            </Menu> */}
+          </div>
+        </div>
+      </div>
+
+      <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-2 pb-3 pt-2">
+          {navigation.map((item) => (
+            <DisclosureButton
+              key={item.name}
+              as="a"
+              href={item.href}
+              aria-current={item.current ? "page" : undefined}
+              className={classNames(
+                item.current
+                  ? "bg-gray-900 text-white"
+                  : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                "block rounded-md px-3 py-2 text-base font-medium"
+              )}
+            >
+              {item.name}
+            </DisclosureButton>
+          ))}
+        </div>
+        <div className="">
+          <CustomButton
+            title="Sign in"
+            containerStyles=" text-white text-sm"
+            handleClick={() => {}}
+          />
+
+          <CustomButton
+            title="Sign Up"
+            containerStyles="text-white text-sm border border-white rounded-full"
+            handleClick={() => {}}
+          />
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
+  );
+}
